60d57ac221d5eb48d1340aac3d3906c89911fec5
Murad Sadigov <myurad5790@gmail.com>
2023-11-27 22:02:54 +0100

    initial commit

---
+++ backend/src/main/java/dev/movieapi/movies/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.movieapi.movies.repository;
+
+import dev.movieapi.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
